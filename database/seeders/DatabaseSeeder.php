c51200e5ade884f41c968a8031de44f65533ecbc
Juan Camilo Gomez <jcami9893@gmail.com>
2026-03-05 10:24:57 -0500

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,19 +1,30 @@
 <?php
 
-namespace Database\Seeders;
-
+use App\Models\CartItem;
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
+
+   
     public function run(): void
     {
-       $this->call([
+        
+       /*$this->call([
         CategorySeeder::class,
         ProductSeeder::class
        ]);
+       */
+
+       //Category::factory(10000)->create();
+       Product::factory(10000)->create();
+       //User::factory(1000)->create();
+       //CartItem::factory(20000)->create();
     }
 }
